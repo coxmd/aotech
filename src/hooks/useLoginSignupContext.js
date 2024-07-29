7e9071b1a79c826f66f65bc754a354570a55db5c
coxmusyoki <coxmusyoki@outlook.com>
2024-07-29 23:27:34 +0300

    Updated Context

--- src/hooks/useLoginSignupContext.js
+++ src/hooks/useLoginSignupContext.js
@@ -2,7 +2,7 @@
 import { useContext } from "react";
 
 //context
-import { LoginSignupContext } from "../contexts/LoginSignupContext";
+import { LoginSignupContext } from "../contexts/LoginsignupContext";
 
 //the hook
 export default function useLoginSignupContext() {
